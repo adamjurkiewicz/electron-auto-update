9d6e1f5723b0f180a83a96a4b8b4fa453880290a
Jurkiewicz Adam <adju@jcommerce.pl>
2021-12-20 16:04:31 +0100

    ain update and restart added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 const { ipcRenderer } = window.require('electron');
 
 function App() {
   const [version, setVersion] = useState('');
+  const [updateAvailable, setUpdateAvailable] = useState(false);
+  const [updateDownloaded, setUpdateDownloaded] = useState(false);
+
+  const restartApp = useCallback( () => {
+    ipcRenderer.send('restart_app');
+  }, [])
 
   useEffect( () => {
     if( ipcRenderer ){
@@ -13,6 +19,14 @@ function App() {
         setVersion(version);
       });
       ipcRenderer.send('app_version');
+      ipcRenderer.on('update_available', () => {
+        ipcRenderer.removeAllListeners('update_available');
+        setUpdateAvailable(true);
+      });
+      ipcRenderer.on('update_downloaded', () => {
+        ipcRenderer.removeAllListeners('update_downloaded');
+        setUpdateDownloaded(true);
+      });
     }else{
       console.log('ipcRenderer missing!');
     }
@@ -22,17 +36,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {`App version: ${version}`}
-        </a>
+        <div>{`App version: ${version}`}</div>
+        { updateAvailable && <div>Downloading update...</div> }
+        { updateDownloaded && <div>
+          <div onClick={restartApp}>Restart application</div>
+        </div> }
       </header>
     </div>
   );
