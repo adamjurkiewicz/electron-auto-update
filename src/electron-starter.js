6012e5d3c499a033400eb2c365b1d3c8153b1ef4
Jurkiewicz Adam <adju@jcommerce.pl>
2021-12-20 16:35:56 +0100

    electron-is-dev added

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,14 +1,16 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
-
+const isDev = require('electron-is-dev');
 const path = require('path');
-const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
+    const url = isDev
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, 'build/index.html')}`;
     // Create the browser window.
     mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
             contextIsolation: false,
@@ -17,7 +19,7 @@ function createWindow() {
         }});
 
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL(url);
 
     // Open the DevTools.
     mainWindow.webContents.openDevTools();
